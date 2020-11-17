1415abf1ca67d4b0cbdc4ef5db5fae1508e71d54
Romain SCHLOTTER <romain.schlotter@gmail.com>
2020-11-17 12:28:13 +0100

    Corrections

--- PHP/Accueil.php
+++ PHP/Accueil.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
     <head>
-        <title>Accuei</title>
+        <title>Accueil</title>
         <meta charset="utf-8">
     </head>
     <body>
@@ -13,7 +13,10 @@
         	<a href="creerStand.php">Création d'un Stand</a>
         </div>
         <div>
-        	<a href="SwitchPresentateurUtilisateur.php">Page Administrateur quant au passage d'un utilisateur en présentateur</a>
+            <a href="SwitchPresentateurUtilisateur.php">Page Administrateur quant au passage d'un utilisateur en présentateur</a>
+        </div>
+        <div>
+            <a href="Connection.php">Connexion</a>
         </div>
     </body>
 </html>
