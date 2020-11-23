0579877da3e655d6d79a51ec78c7738b034b825f
AnthonyLamour <a.lamour@ludus-academie.com>
2020-11-23 11:35:03 +0100

    session v2

--- PHP/Site.php
+++ PHP/Site.php
@@ -8,13 +8,15 @@
         <h1>Site</h1>
     <footer>
 		<?php
-
+			require_once("PHP/objUtilisateur.php");
 			if(!isset($_SESSION['user_name'])){
 				session_start();
+				$tmpUtilisateur = insertTempUtilisateur()
+				$_SESSION['user_name']=$tmpUtilisateur["Nom"];
 
-				$_SESSION['user_name']="guestTest";
+				$_SESSION['user_ID']=$tmpUtilisateur["Id"];
 
-				$_SESSION['user_ID']=0;
+				$_SESSION['user_Statue']=0;
 
 			}else{
 				echo "vous êtes : ".$_SESSION['user_name'];
