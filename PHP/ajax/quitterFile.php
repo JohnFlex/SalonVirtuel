e8c7be50c4c63f3da07444bdfada66cad696823e
Romain SCHLOTTER <romain.schlotter@gmail.com>
2020-12-01 11:41:47 +0100

    Correction file d'attente

--- PHP/ajax/quitterFile.php
+++ PHP/ajax/quitterFile.php
@@ -1,6 +1,6 @@
 <?php
 // Ce fichier reçoit les données en json et enregistre le message
-require_once("../COMM/Add_In_Queue.php");
+//require_once("../COMM/Add_In_Queue.php");
 
 // On vérifie la méthode
 if($_SERVER['REQUEST_METHOD'] == 'GET')
@@ -12,16 +12,16 @@ if($_SERVER['REQUEST_METHOD'] == 'GET')
         $db = ConnexionBDD();
 
         // On écrit la requête
-        $sql = 'DELETE FROM DB_SALON_Reunions WHERE ID_Avatar = '.$_GET["ID_Utilisateur"].';';
+        $sql = 'DELETE FROM DB_SALON_Reunions WHERE ID_Avatar = '.$_GET["UserId"].';';
 
         // On exécute la requête
         $query = $db->query($sql);
 
         // On récupère les données
-        $resultat = $query->fetchAll();
+        //$resultat = $query->fetchAll();
 
         // On encode en JSON
-        $resultatJson = json_encode($resultat);
+        $resultatJson = json_encode(/*$resultat*/"Reussite");
 
         // On envoie
         echo $resultatJson;
